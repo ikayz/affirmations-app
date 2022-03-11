699704a6252fd6d4fcbc422dd4a9ffeadc39ff15
Isaac Miti <ikayz@live.com>
2022-03-11 11:51:33 +0200

    Complete the item view holder

--- app/src/main/java/com/ikayz/kotlin/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ikayz/kotlin/affirmations/adapter/ItemAdapter.kt
@@ -14,18 +14,25 @@ class ItemAdapter(
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+
+    // Provide a reference to the views for each data item
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    // Create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // Create new view
         val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
+    // Replace the contents of a view (invoked by Layout manager)
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 
+    // Return the size of the dataset
     override fun getItemCount() = dataset.size
 }
