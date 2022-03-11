64dadeb73f010249f5a04d02aa25f89ff80898eb
Isaac Miti <ikayz@live.com>
2022-03-11 11:58:21 +0200

    Finish implementing the recycler view

--- app/src/main/java/com/ikayz/kotlin/affirmations/MainActivity.kt
+++ app/src/main/java/com/ikayz/kotlin/affirmations/MainActivity.kt
@@ -3,11 +3,23 @@ package com.ikayz.kotlin.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ikayz.kotlin.affirmations.adapter.ItemAdapter
 import com.ikayz.kotlin.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialise the data
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+
     }
 }
