b9fb0cd89a9b48904576777ae220f85851c74197
Isaac Miti <ikayz@live.com>
2022-03-22 23:38:55 +0200

    Add list of data to test

--- app/src/test/java/com/ikayz/kotlin/affirmations/AffirmationsAdapterTests.kt
+++ app/src/test/java/com/ikayz/kotlin/affirmations/AffirmationsAdapterTests.kt
@@ -1,6 +1,7 @@
 package com.ikayz.kotlin.affirmations
 
 import android.content.Context
+import com.ikayz.kotlin.affirmations.model.Affirmation
 import org.junit.Test
 import org.mockito.MockedConstruction
 import org.mockito.Mockito.mock
@@ -8,5 +9,11 @@ import org.mockito.Mockito.mock
 class AffirmationsAdapterTests {
     private val context = mock(Context::class.java)
 
-
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+    }
 }
