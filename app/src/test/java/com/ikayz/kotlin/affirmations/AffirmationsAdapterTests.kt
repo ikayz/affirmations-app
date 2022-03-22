6c94b3f33c22f3c07ba2060e0b0548f16bf10d28
Isaac Miti <ikayz@live.com>
2022-03-22 23:36:50 +0200

    Add unit test

--- app/src/test/java/com/ikayz/kotlin/affirmations/AffirmationsAdapterTests.kt
+++ app/src/test/java/com/ikayz/kotlin/affirmations/AffirmationsAdapterTests.kt
@@ -1,4 +1,12 @@
 package com.ikayz.kotlin.affirmations
 
+import android.content.Context
+import org.junit.Test
+import org.mockito.MockedConstruction
+import org.mockito.Mockito.mock
+
 class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+
 }
